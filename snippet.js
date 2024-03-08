e0bc74d17d438338c59abcfdbdf8edd060b2c0ce
Marco Gazerro <gazerro@open2b.com>
2024-03-08 17:09:42 +0100

    Restore semicolons in the snippet
    Semicolons were removed in the JavaScript SDK, but erroneously, they
    were also removed for the snippet. This change restores them.
    
    Additionally, set the path of 'chichi.min.js' as absolute.

--- snippet.js
+++ snippet.js
@@ -1,20 +1,20 @@
 !(function () {
-	var a = (window.chichianalytics = window.chichianalytics || [])
+	var a = (window.chichianalytics = window.chichianalytics || []);
 	if (a.load) {
-		window.console && console.error && console.error('The ChiChi snippet is included twice')
-		return
+		window.console && console.error && console.error('The ChiChi snippet is included twice');
+		return;
 	}
 	a.load = function (key, url, options) {
-		a.key = key
-		a.url = url
-		a.options = options
-		var s = document.createElement('script')
-		s.async = !0
-		s.type = 'text/javascript'
-		s.src = '../dist/chichi.min.js'
-		var c = document.getElementsByTagName('script')[0]
-		c.parentNode.insertBefore(s, c)
-	}
+		a.key = key;
+		a.url = url;
+		a.options = options;
+		var s = document.createElement('script');
+		s.async = !0;
+		s.type = 'text/javascript';
+		s.src = '/javascript-sdk/dist/chichi.min.js';
+		var c = document.getElementsByTagName('script')[0];
+		c.parentNode.insertBefore(s, c);
+	};
 	var methods = [
 		'alias',
 		'anonymize',
@@ -32,15 +32,14 @@
 		'startSession',
 		'track',
 		'user',
-	]
+	];
 	for (var i = 0; i < methods.length; i++) {
-		;(function (name) {
+		(function (name) {
 			a[name] = function () {
-				a.push([name].concat(Array.prototype.slice.call(arguments)))
-				return a
-			}
-		})(methods[i])
+				a.push([name].concat(Array.prototype.slice.call(arguments)));
+				return a;
+			};
+		})(methods[i]);
 	}
-	a.load('kxe7WIDDGvcfDEKgHePfHzuHQ6dTU2xc', 'https://localhost:9090/api/v1/')
-	a.page()
-})()
+	a.load('kxe7WIDDGvcfDEKgHePfHzuHQ6dTU2xc', 'https://localhost:9090/api/v1/');
+})();
